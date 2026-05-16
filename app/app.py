62e6e31008f28cf6b71171831bdd27a16510b3ca
vamsi-kanakam <vamsikanakam2002@gmail.com>
2026-05-16 11:30:20 -0500

    delete api end-point added.

--- app/app.py
+++ app/app.py
@@ -85,4 +85,23 @@ async def get_feed(session: AsyncSession = Depends(get_async_session)):
         )
     return {'posts': posts_data}
 
+@app.delete('/posts/{post_id}')
+async def delete_post(post_id: str, session: AsyncSession = Depends(get_async_session)):
+    try:
+        post_uuid = uuid.UUID(post_id)
+        result = await session.execute(select(Post).where(Post.id == post_uuid))
+        post = result.scalars().first()
 
+        if not post:  
+            raise HTTPException(status_code=404, detail="Post not found")
+        
+        # file_id = os.path.basename(post.url)
+        # delete_result = imagekit.files.delete(file_id)
+        # print(f"Delete result for file ID {file_id}: {delete_result}")
+
+        await session.delete(post)
+        await session.commit()
+        return {"success":True ,"message": "Post deleted successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+     
